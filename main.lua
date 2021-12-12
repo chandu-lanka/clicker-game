2ba0f95af223a2d7e0fe3a3ca44ef4bc8b08f6a5
TriangleDevv <chandu.lanka13@gmail.com>
2021-12-12 14:43:40 +0530

    got a game_state working so now we have a menu state and a game state

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ function love.load()
     target:load()
 
     font = love.graphics.newFont("data/fonts/m5x7.ttf", 45)
+    title_font = love.graphics.newFont("data/fonts/m5x7.ttf", 100)
 
     score = 0
     timer = 0
@@ -21,12 +22,18 @@ function love.update(dt)
 end
 
 function love.mousepressed(x, y, button)
-    if button == 1 then
-        if mouse_x > target.x and mouse_x < target.x + target.collider.width then
-            if mouse_y > target.y and mouse_y < target.y + target.collider.height then
-                score = score + 1
-                target.x = math.random(target.collider.width, love.graphics.getWidth() - target.collider.width)
-                target.y = math.random(target.collider.height, love.graphics.getHeight() - target.collider.height)
+    if game_state == 1 then
+        if button == 1 then
+            game_state = 2
+        end
+    elseif game_state == 2 then
+        if button == 1 then
+            if mouse_x > target.x and mouse_x < target.x + target.collider.width then
+                if mouse_y > target.y and mouse_y < target.y + target.collider.height then
+                    score = score + 1
+                    target.x = math.random(target.collider.width, love.graphics.getWidth() - target.collider.width)
+                    target.y = math.random(target.collider.height, love.graphics.getHeight() - target.collider.height)
+                end
             end
         end
     end
@@ -34,12 +41,17 @@ end
 
 function love.draw()
     if game_state == 1 then
-        
+        love.graphics.setFont(title_font)
+        love.graphics.print("Clicker Game", 200, 100)
+        love.graphics.setFont(font)
+        love.graphics.print("Click Anywhere To Play", 230, 300)
+
+    elseif game_state == 2 then
+        love.graphics.setBackgroundColor(0.2,0.2,0.2,1)
+        target:draw()
+        love.graphics.setFont(font)
+        love.graphics.print("Score: "..tostring(score), 0, 0)
     end
-    
-    love.graphics.setBackgroundColor(0.2,0.2,0.2,1)
-    target:draw()
-    love.graphics.setFont(font)
-    love.graphics.print("Score: "..tostring(score), 0, 0)
+
     player:draw()
 end
